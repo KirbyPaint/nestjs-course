6af259cb4645d617e37add649ee28e349ef0cfec
Ashley Porter <porter.ashley13@gmail.com>
2021-06-06 14:22:58 -0700

    add lesson 5 content - Error Handling: Username Conflicts

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,3 +1,4 @@
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 import { EntityRepository, Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { User } from "./user.entity";
@@ -9,6 +10,16 @@ export class UsersRepository extends Repository<User> {
 
     const user = this.create({ username, password });
 
-    await this.save(user);
+    try {
+      await this.save(user);
+    }
+    catch (error) {
+      if (error.code === '23505') { // Duplicate Username
+        throw new ConflictException('Username already exists');
+      }
+      else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
